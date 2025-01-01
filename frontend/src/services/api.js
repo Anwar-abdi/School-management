d6cda4c5d1f6012af48f8a8049dfa3d015cd0a76
Munasrr <anwarabdi2090@gmail.com>
2025-01-01 20:11:06 +0300

    added api

---
+++ frontend/src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Student endpoints
+export const getStudents = () => api.get('/students');
+export const createStudent = (data) => api.post('/students', data);
+export const updateStudent = (id, data) => {
+  console.log('Making update request:', { id, data });
+  return api.put(`/students/${id}`, data);
+};
+export const deleteStudent = (id) => api.delete(`/students/${id}`);
+
+// Teacher endpoints
+export const getTeachers = () => api.get('/teachers');
+export const createTeacher = (data) => api.post('/teachers', data);
+export const updateTeacher = (id, data) => api.put(`/teachers/${id}`, data);
+export const deleteTeacher = (id) => api.delete(`/teachers/${id}`);
