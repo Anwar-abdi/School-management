3e9e6081f732eff6af307ca8174e757d1cfc2f7b
Munasrr <anwarabdi2090@gmail.com>
2025-01-01 19:09:53 +0300

    student schema

---
+++ backend/models/student.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema(
+  {
+    ID: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email'],
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ['male', 'female'],
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return (
+            v <= new Date() &&
+            v >=
+              new Date(new Date().setFullYear(new Date().getFullYear() - 100))
+          );
+        },
+        message: 'Invalid date of birth',
+      },
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        ret.dateOfBirth = ret.dateOfBirth.toISOString().split('T')[0];
+        return ret;
+      },
+    },
+  }
+);
+
+module.exports = mongoose.model('Student', studentSchema);
